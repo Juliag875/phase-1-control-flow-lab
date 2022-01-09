0f6cd7c3d8e0744d5e7513af873fa222057a73ef
Iuliia El Sammak <juliag875@gmail.com>
2022-01-09 10:58:28 -0500

    Done

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ function scuberGreetingForFeet(ride){
   let greeting;
   if (ride <= 400) {
     greeting = "This one is on me!";
-  } else if (ride >= 2000 && ride <= 2499) {
+  } else if (ride >= 2000 && ride <= 2500) {
     greeting = "I will gladly take your thirty bucks.";
-  } else if (ride >= 2500) {
+  } else if (ride > 2500) {
     greeting = "No can do.";
   }
   return greeting;
